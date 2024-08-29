93063253000353447f3b1465613232b593373ff3
ArhumsCode <122049956+ArhumsCode@users.noreply.github.com>
2024-08-28 22:12:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
-print('hello world')
-print('hello world')
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from joblib import dump, load
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+feature_names = iris.feature_names
+target_names = iris.target_names
+
+print(target_names)
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print(X_train.shape)
+print(X_test.shape)
+
+knn = KNeighborsClassifier(n_neighbors=3)
+
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+
+from sklearn import metrics
+print(metrics.accuracy_score(y_test, y_pred))
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=3)
+
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+
+from sklearn import metrics
+print(metrics.accuracy_score(y_test, y_pred))
+
+model = dump(knn, 'mlbrain.joblib')
